db2b21e4c144b3e668d94dbd5947a93d5077afc1
awebisam <abisamism@gmail.com>
2021-11-27 19:33:18 +0545

    minor documentation

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,8 +2,16 @@ package config
 
 import "html/template"
 
+// AppConfig TODO: Add more config options
 // AppConfig Holds the application config items
 type AppConfig struct {
-	Debug         bool // Debug mode
+	Debug bool
+	/* When Debug is true:
+	1. Templates are cached on every request that means that the application
+		will be slower, but you don't need to restart the server everytime you
+		make changes on template.
+	2. TODO: Add more usage on Debug mode
+	*/
+
 	TemplateCache map[string]*template.Template
 }
